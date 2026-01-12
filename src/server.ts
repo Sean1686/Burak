9a17f4ab80d42cd9745cca8051346573d19233cf
Sean1686 <khudayberdi11@gmail.com>
2026-01-12 19:17:14 +0900

    BRR: Burak loyihasi

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+console.log("ts compiled file running");
+
+import moment from "moment";
+const currentTime = moment().format("Y M D");
+console.log(currentTime);
+const persn: string = "Nathan";
+const count: number = 200;
