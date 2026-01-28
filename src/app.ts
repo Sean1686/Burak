caca2a7a65a7ca5db08d5425e4521817c93d8a33
Sean1686 <khudayberdi11@gmail.com>
2026-01-29 03:06:37 +0900

    feat: Integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,28 @@ import Memberservise from "./models/Member.servise";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
-/** 1-entrance */
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGODB_URL),
+  collection: 'mySessions'
+});
+
+/** 1-ENTRANCE */
 const app = express();
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended: true})); // HTML form kelayotgan requestlarni parse(tartib bilan orgniga qoyib beradi) qilish uchun
 app.use(express.json()); // json formatdagi kelayotgan requestlarni obyektga ozgartirish uchun
 app.use(morgan(MORGAN_FORMAT));
-/** 2-session */
+/** 2-SESSION */
 
-/** 3-views */
+/** 3-VIEWS */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-/** 4-routers */
+/** 4-ROUTERS */
 app.use("/admin", routerAdmin); // SSR: EJS
 app.use("/", router); // SPA: REACT
 
